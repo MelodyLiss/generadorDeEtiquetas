3c35c5da31d49a20cd86d1b66bd80fe6f60d3187
MelodyLiss <lisette.jofre.1@gmail.com>
2025-05-15 20:13:03 -0400

    eliminacion prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import './App.css'
 import { Home } from './pages/Home'
-import { PaginaPrueba } from './pruebas/PaginaPrueba'
 import { EtiquetaProvider } from "./context/EtiquetaProvider.jsx"
 
 function App() {
