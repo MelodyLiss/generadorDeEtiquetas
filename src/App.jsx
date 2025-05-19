b33ffbe458188f3f8a4426785dea43386632baea
MelodyLiss <lisette.jofre.1@gmail.com>
2025-05-19 14:50:54 -0400

    fix:home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Home } from './pages/Home.jsx'
+import { Home } from '@/pages/Home'
 import { EtiquetaProvider } from "./context/EtiquetaProvider.jsx"
 
 function App() {
